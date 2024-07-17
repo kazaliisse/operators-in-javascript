2bbf2100c542b44d571ded14f5c6cc5d6a655438
noor <noorhafowbare@gmail.com>
2024-07-17 15:57:03 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// math operators
+const now = 2037;
+const ageNoor = now - 2020;
+const ageIsse = now - 1957;
+console.log(ageNoor, ageIsse);
+
+console.log(ageNoor * 2, ageNoor / 10, 2 ** 3);
+
+
+const firstName = "Noor";
+const lastName = "Isse";
+console.log(firstName + " " + lastName);
+// assigment operators
+let x = 10 + 5;
+x += 10;
+x *= 4
+x ++;
+x --; 
+console.log(x)
+
+// comparisson operators
+console.log(ageIsse > ageNoor); 
+// >, <, >= <=
+console.log(ageNoor >= 18);
